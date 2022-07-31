a0510d5c50e4744089bba6191027c8dc7bdc7fd3
shizimi50 <mjrwrbbc@gmail.com>
2022-07-31 23:56:10 +0900

    feat: i18nを設定し、英語と日本語の翻訳を追加

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,14 @@ module BooksApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    # 言語ファイル参照先パス
+    I18n.load_path += Dir[Rails.root.join("config/locales/*.{rb,yml}")]
+
+    # 使用する言語
+    config.i18n.available_locales = %i(ja en)
+    
+    # デフォルト言語を指定
+    config.i18n.default_locale = :ja
 
     # Configuration for the application, engines, and railties goes here.
     #
